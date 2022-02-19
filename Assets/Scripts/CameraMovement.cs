3da830dc572b86af381542bb41f3d86dcb83b59c
unknown <victormgb3099@gmail.com>
2022-02-19 15:52:55 -0400

    diseno de nivel progresado, ahora a crear script de ataque

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+
+    public Transform target;
+    public float smoothing;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,13 @@ public class CameraMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+        }
     }
 }
